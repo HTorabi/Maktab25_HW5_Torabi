e42c90c634a2de4c407db0ab23a76b4f4989b551
unknown <hamid.r_torabi@yahoo.com>
2019-03-22 14:28:01 +0430

    Create subclass FormalShoes from superclass Shoes

---
+++ FormalShoes.java
@@ -0,0 +1,6 @@
+public class FormalShoes extends Shoes {
+
+    public FormalShoes(String type, String producer, long price, int number, Color color) {
+        super(type, producer, price, number, color);
+    }
+}
