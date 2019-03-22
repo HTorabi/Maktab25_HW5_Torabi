b71f0bd2a3f28f0cdec874974b29b4c5afb742b6
unknown <hamid.r_torabi@yahoo.com>
2019-03-22 14:18:04 +0430

    Create subclass Radio

---
+++ Radio.java
@@ -0,0 +1,5 @@
+public class Radio extends ElectricalDevices {
+    public Radio(String type, String producer, long price, int number, String color) {
+        super(type, producer, price, number, color);
+    }
+}
