da16cb98405b6d9327c2527681f30f0ec0cdaff5
unknown <hamid.r_torabi@yahoo.com>
2019-03-22 20:33:31 +0430

    Adds writer

--- Book.java
+++ Book.java
@@ -1,5 +1,8 @@
 public class Book extends Readable {
-    public Book(String type, String producer, long price, int number, String name) {
+    String writter;
+
+    public Book(String type, String producer, long price, int number, String name, String writter) {
         super(type, producer, price, number, name);
+        this.writter = writter;
     }
 }
