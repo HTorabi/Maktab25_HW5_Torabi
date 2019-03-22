d0a1906697e9cb9ec5b6f868c3c71b35b9c55f21
unknown <hamid.r_torabi@yahoo.com>
2019-03-22 14:29:38 +0430

    Create subclass Magazine from superclass Readable

---
+++ Magazine.java
@@ -0,0 +1,5 @@
+public class Magazine extends Readable {
+    public Magazine(String type, String producer, long price, int number, String name) {
+        super(type, producer, price, number, name);
+    }
+}
