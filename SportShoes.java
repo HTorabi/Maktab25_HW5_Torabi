27ab32cb458e62b7ad6da360cc8b4161d388465b
unknown <hamid.r_torabi@yahoo.com>
2019-03-22 14:28:38 +0430

    Create subclass SportShoes from superclass Shoes

---
+++ SportShoes.java
@@ -0,0 +1,5 @@
+public class SportShoes extends Shoes {
+    public SportShoes(String type, String producer, long price, int number, Color color) {
+        super(type, producer, price, number, color);
+    }
+}
