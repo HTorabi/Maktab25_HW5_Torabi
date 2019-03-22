7a0d184eef70f8f1a0aa727b6617aa8c1e70f59e
unknown <hamid.r_torabi@yahoo.com>
2019-03-22 12:36:22 +0430

    Adds a constructor and removes setter/getter

--- User.java
+++ User.java
@@ -1,68 +1,25 @@
 public class User {
+    String city;
+    String town;
+    String streetName;
     private String ID;
     private long password;
     private String firstName;
     private String lastName;
     private long phoneNumber;
     private String email;
-    String city;
-    String town;
-    String streetName;
     private long postalCode;
 
-    public String getID() {
-        return ID;
-    }
-
-    public void setID(String ID) {
+    public User(String ID, long password, String firstName, String lastName, long phoneNumber, String email, String city, String town, String streetName, long postalCode) {
         this.ID = ID;
-    }
-
-    public long getPassword() {
-        return password;
-    }
-
-    public void setPassword(long password) {
         this.password = password;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
         this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
         this.lastName = lastName;
-    }
-
-    public long getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(long phoneNumber) {
         this.phoneNumber = phoneNumber;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
         this.email = email;
-    }
-
-    public long getPostalCode() {
-        return postalCode;
-    }
-
-    public void setPostalCode(long postalCode) {
+        this.city = city;
+        this.town = town;
+        this.streetName = streetName;
         this.postalCode = postalCode;
     }
 }
