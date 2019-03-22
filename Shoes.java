9cffeecaeb90907cb0c880d82be8c670e7a2a83d
unknown <hamid.r_torabi@yahoo.com>
2019-03-22 14:07:42 +0430

    Puts a constructor and Enum for color

--- Shoes.java
+++ Shoes.java
@@ -1,11 +1,10 @@
 public class Shoes extends Goods {
-    private String color;
+    Color color;
 
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
+    public Shoes(String type, String producer, long price, int number, Color color) {
+        super(type, producer, price, number);
         this.color = color;
     }
+
+    enum Color {BLACK, WHITE, BROWN,GREY}
 }
