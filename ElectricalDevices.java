ddd39fbe73f1d805ea2411baccf575265cbb50b5
unknown <hamid.r_torabi@yahoo.com>
2019-03-22 11:28:33 +0430

    Adds a class for Electrca devices that inherits Goods

---
+++ ElectricalDevices.java
@@ -0,0 +1,29 @@
+public class ElectricalDevices extends Goods {
+    private String type;
+    private String color;
+    private String manufacturer;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+}
