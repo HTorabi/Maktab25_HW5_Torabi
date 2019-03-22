7c9149427b1569d763132f633128cad568986131
unknown <hamid.r_torabi@yahoo.com>
2019-03-22 14:18:45 +0430

    Create subclass Television

---
+++ Television.java
@@ -0,0 +1,5 @@
+public class Television extends ElectricalDevices {
+    public Television(String type, String producer, long price, int number, String color) {
+        super(type, producer, price, number, color);
+    }
+}
