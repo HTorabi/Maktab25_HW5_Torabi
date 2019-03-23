72d9abec92f2ca8814d4463311e1d874be43537d
unknown <hamid.r_torabi@yahoo.com>
2019-03-23 10:58:44 +0430

    Prints the goods features

--- Main.java
+++ Main.java
@@ -26,18 +26,21 @@ public class Main {
         String postalCode=scanner.nextLine();
         new UserAddress(city,town,streetName,postalCode);
         for (Goods g:GoodsList.goods){
-            System.out.println(g.type);
-            System.out.println(g.number);
-            System.out.println(g.producer);
-            System.out.println(g.price);
+            System.out.println("Type: "+g.type);
+
+            if (g instanceof Shoes)
+                System.out.println("Color: "+((Shoes) g).color);
+            if (g instanceof ElectricalDevices)
+                System.out.println("Color: "+((ElectricalDevices)g).color);
+            if (g instanceof Book)
+                System.out.println("Name: "+((Book) g).name+"\n"+"Writer: "+((Book) g).writter);
+            if (g instanceof Magazine)
+                System.out.println("Name: "+((Magazine) g).name+"\n");
+            System.out.println("Producer: "+g.producer);
+            System.out.println("Price: "+g.price);
+            System.out.println("Number: "+g.number);
+
             System.out.println("======================");
         }
-
-
-
-
-
-
-
     }
 }
