87fc0092a96d50e6bf12eb8c7274391dd341d154
unknown <hamid.r_torabi@yahoo.com>
2019-03-23 11:52:17 +0430

    Create a class for showing the goods

---
+++ GoodsShow.java
@@ -0,0 +1,21 @@
+public class GoodsShow {
+    GoodsShow(){
+        for (Goods g:GoodsList.goods){
+            System.out.println(g.ID);
+            System.out.println("Type: "+g.type);
+
+            if (g instanceof Shoes)
+                System.out.println("Color: "+((Shoes) g).color);
+            if (g instanceof ElectricalDevices)
+                System.out.println("Color: "+((ElectricalDevices)g).color);
+            if (g instanceof Book)
+                System.out.println("Name: "+((Book) g).name+"\n"+"Writer: "+((Book) g).writter);
+            if (g instanceof Magazine)
+                System.out.println("Name: "+((Magazine) g).name+"\n");
+            System.out.println("Producer: "+g.producer);
+            System.out.println("Price: "+g.price);
+            System.out.println("Number: "+g.number);
+
+            System.out.println("======================");
+    }
+}}
