7bf05439a11fface6e36c2cc7f8a9d5caa0f461f
unknown <hamid.r_torabi@yahoo.com>
2019-03-23 10:25:36 +0430

    Get information from user and show him a list of goods

--- Main.java
+++ Main.java
@@ -1,2 +1,43 @@
+import java.util.Scanner;
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Please Enter Your ID");
+        String ID=scanner.nextLine();
+        System.out.println("Please Enter Your Password");
+        String password=scanner.nextLine();
+        new UserRegistration(ID,password);
+        System.out.println("Please Enter Your first name");
+        String firstName=scanner.nextLine();
+        System.out.println("Please Enter Your last name");
+        String lastName=scanner.nextLine();
+        System.out.println("Please Enter Your Phone number");
+        String phoneNumber=scanner.nextLine();
+        System.out.println("Please Enter Your Email");
+        String email=scanner.nextLine();
+        new UserInformation(firstName,lastName,phoneNumber,email);
+        System.out.println("Please Enter Your City name");
+        String city=scanner.nextLine();
+        System.out.println("Please Enter Your Town name");
+        String town=scanner.nextLine();
+        System.out.println("Please Enter Your Street name");
+        String streetName=scanner.nextLine();
+        System.out.println("Please Enter Your Postal Code");
+        String postalCode=scanner.nextLine();
+        new UserAddress(city,town,streetName,postalCode);
+        for (Goods g:GoodsList.goods){
+            System.out.println(g.type);
+            System.out.println(g.number);
+            System.out.println(g.producer);
+            System.out.println(g.price);
+            System.out.println("======================");
+        }
+
+
+
+
+
+
+
+    }
 }
