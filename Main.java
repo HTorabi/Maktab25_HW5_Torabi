bfea97009cd540931c65ea51f57b8d3eb7650af3
unknown <hamid.r_torabi@yahoo.com>
2019-03-23 18:02:39 +0430

    Adds an array and some features for removing and finalize the box

--- Main.java
+++ Main.java
@@ -28,11 +28,51 @@ public class Main {
         new UserAddress(city, town, streetName, postalCode);
         for (Goods g : GoodsList.goods)
             new GoodsShow(g);
+        Goods[] item = new Goods[5];
+        int i = 0;
+        while (true) {
+
+            System.out.println("Enter Code of goods which you want");
+            int id = scanner.nextInt();
+            System.out.println("Enter Number of this");
+            int num = scanner.nextInt();
+            System.out.println("Your Shopping BOX");
+//            item[i] = new ShoppingBox(id, num);
+            for (Goods g : GoodsList.goods) {
+                if (g.ID == id) {
+                    g.number -= num;
+                    item[i] = g;
+                }
+            }
+            System.out.println("Do you want to remove any Item? y/n");
+            Scanner scanner1 = new Scanner(System.in);
+            String yn = scanner1.nextLine();
+            if (yn.equalsIgnoreCase("y")) {
+                System.out.println("For removing any item Enter its code");
+                int code = scanner.nextInt();
+                for (int j = 0; j <= i; j++)
+                    if (item[j].ID == code)
+                        for (int k = 1; k < i; k++)
+                            if (j + k < 5)
+                                item[j] = item[j + k];
+            }
+            if (yn.equalsIgnoreCase("n")) {
+                i++;
+                if (i == 5) {
+                    System.out.println("Maximum item is 5");
+                    break;
+                }
 
 
-        System.out.println("Enter Code of goods which you want");
-        int id = scanner.nextInt();
-        System.out.println("Enter Number of this");
-        int num = scanner.nextInt();
+            }
+            System.out.println("if you want to finalize your box Enter Exit");
+            Scanner scanner2 = new Scanner(System.in);
+            if (scanner2.nextLine().equalsIgnoreCase("exit"))
+                break;
+
+        }
+        for (Goods it : item)
+            if (it != null)
+                new GoodsShow(it);
     }
 }
