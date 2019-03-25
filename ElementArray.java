2332bcf35367dd8dd36a1c73cfd367f90a7ebba1
unknown <hamid.r_torabi@yahoo.com>
2019-03-25 10:21:05 +0430

    Adds a loop for receiving element

--- ElementArray.java
+++ ElementArray.java
@@ -3,36 +3,44 @@ import java.util.Scanner;
 public class ElementArray {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Element[] element1 = new MetalElement[2];
-        Element[] element2 = new NonMetalElement[2];
-        int i=0,j=0;
-        while (true){
+        Element[] element = new MetalElement[5];
+        int i = 0, m = 0, n = 0;
+        while (true) {
+            if (n == 3)
+                System.out.println("We need a metal");
+            if (m == 3)
+                System.out.println("We need a Nonmetal");
             System.out.println("Metal or NonMetal?");
             if (scanner.nextLine().equalsIgnoreCase("Metal")) {
 
-                element1[i]=new MetalElement(elementProducer()[0],Integer.parseInt(elementProducer()[1]),
+                element[i] = new MetalElement(elementProducer()[0], Integer.parseInt(elementProducer()[1]),
                         Integer.parseInt(elementProducer()[2]));
                 i++;
+                m++;
             }
             if (scanner.nextLine().equalsIgnoreCase("NonMetal")) {
 
-                element2[j]=new NonMetalElement(elementProducer()[0],Integer.parseInt(elementProducer()[1]),
+                element[i] = new NonMetalElement(elementProducer()[0], Integer.parseInt(elementProducer()[1]),
                         Integer.parseInt(elementProducer()[2]));
-                j++;
+                i++;
+                n++;
             }
+            if (i==5)
+                break;
 
 
         }
     }
-    static String[] elementProducer(){
-        String[] ep=new String[3];
+
+    static String[] elementProducer() {
+        String[] ep = new String[3];
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter its symbol");
-        ep[0]=scanner.nextLine();
+        ep[0] = scanner.nextLine();
         System.out.println("Enter its Atomic number");
-        ep[1]=scanner.nextLine();
+        ep[1] = scanner.nextLine();
         System.out.println("Enter its Atomic Weight");
-        ep[2]=scanner.nextLine();
+        ep[2] = scanner.nextLine();
         return ep;
 
     }
