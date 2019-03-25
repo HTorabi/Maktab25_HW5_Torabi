43dc428e79763a4e01a418ee5bffc1c3fe642927
unknown <hamid.r_torabi@yahoo.com>
2019-03-25 11:07:02 +0430

    Adds a condition for numbers>0

--- GoodsShow.java
+++ GoodsShow.java
@@ -1,20 +1,22 @@
 public class GoodsShow {
     GoodsShow(Goods g){
-            System.out.println("Code: "+g.ID);
-            System.out.println("Type: "+g.type);
+        if (g.number>0) {
+            System.out.println("Code: " + g.ID);
+            System.out.println("Type: " + g.type);
 
             if (g instanceof Shoes)
-                System.out.println("Color: "+((Shoes) g).color);
+                System.out.println("Color: " + ((Shoes) g).color);
             if (g instanceof ElectricalDevices)
-                System.out.println("Color: "+((ElectricalDevices)g).color);
+                System.out.println("Color: " + ((ElectricalDevices) g).color);
             if (g instanceof Book)
-                System.out.println("Name: "+((Book) g).name+"\n"+"Writer: "+((Book) g).writter);
+                System.out.println("Name: " + ((Book) g).name + "\n" + "Writer: " + ((Book) g).writter);
             if (g instanceof Magazine)
-                System.out.println("Name: "+((Magazine) g).name+"\n");
-            System.out.println("Producer: "+g.producer);
-            System.out.println("Price: "+g.price);
-            System.out.println("Number: "+g.number);
+                System.out.println("Name: " + ((Magazine) g).name + "\n");
+            System.out.println("Producer: " + g.producer);
+            System.out.println("Price: " + g.price);
+            System.out.println("Number: " + g.number);
 
             System.out.println("======================");
+        }
     }
 }
