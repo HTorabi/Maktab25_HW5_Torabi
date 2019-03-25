875dc917bcb01ba1644bd02b322b2abb85b6a4e8
unknown <hamid.r_torabi@yahoo.com>
2019-03-25 10:06:39 +0430

    Changes methode to String array and call it for each metal and nonmetal

--- ElementArray.java
+++ ElementArray.java
@@ -2,27 +2,38 @@ import java.util.Scanner;
 
 public class ElementArray {
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        Element[] element1=new MetalElement[2];
-        Element[] element2=new NonMetalElement[2];
-        for (int i=0;i<2;i++) {
+        Scanner scanner = new Scanner(System.in);
+        Element[] element1 = new MetalElement[2];
+        Element[] element2 = new NonMetalElement[2];
+        int i=0,j=0;
+        while (true){
             System.out.println("Metal or NonMetal?");
-            if (scanner.nextLine().equalsIgnoreCase("Metal")){
+            if (scanner.nextLine().equalsIgnoreCase("Metal")) {
 
-
-
+                element1[i]=new MetalElement(elementProducer()[0],Integer.parseInt(elementProducer()[1]),
+                        Integer.parseInt(elementProducer()[2]));
+                i++;
             }
+            if (scanner.nextLine().equalsIgnoreCase("NonMetal")) {
+
+                element2[j]=new NonMetalElement(elementProducer()[0],Integer.parseInt(elementProducer()[1]),
+                        Integer.parseInt(elementProducer()[2]));
+                j++;
+            }
+
 
         }
     }
-    void elementInfo(){
-        Scanner scanner=new Scanner(System.in);
-        Scanner scanner1=new Scanner(System.in);
+    static String[] elementProducer(){
+        String[] ep=new String[3];
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter its symbol");
-        String symbol=scanner.nextLine();
+        ep[0]=scanner.nextLine();
         System.out.println("Enter its Atomic number");
-        int atomicNum=scanner1.nextInt();
+        ep[1]=scanner.nextLine();
         System.out.println("Enter its Atomic Weight");
-        int atomicWeight=scanner1.nextInt();
+        ep[2]=scanner.nextLine();
+        return ep;
+
     }
 }
