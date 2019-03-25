298a146f5fb39e747afa86404bd7f093b7b3f473
unknown <hamid.r_torabi@yahoo.com>
2019-03-25 12:41:45 +0430

    Adds a methode for showing goods and additional parameter in constructor according to where we call constructor

--- GoodsShow.java
+++ GoodsShow.java
@@ -1,22 +1,34 @@
 public class GoodsShow {
-    GoodsShow(Goods g){
-        if (g.number>0) {
-            System.out.println("Code: " + g.ID);
-            System.out.println("Type: " + g.type);
+    GoodsShow(Goods g,int where) {
+        if (where == 0) {
+            if (g.number >= 0) {
+                goodsShow(g);
 
-            if (g instanceof Shoes)
-                System.out.println("Color: " + ((Shoes) g).color);
-            if (g instanceof ElectricalDevices)
-                System.out.println("Color: " + ((ElectricalDevices) g).color);
-            if (g instanceof Book)
-                System.out.println("Name: " + ((Book) g).name + "\n" + "Writer: " + ((Book) g).writter);
-            if (g instanceof Magazine)
-                System.out.println("Name: " + ((Magazine) g).name + "\n");
-            System.out.println("Producer: " + g.producer);
-            System.out.println("Price: " + g.price);
-            System.out.println("Number: " + g.number);
+            }
+        } else {
+            if (g.number > 0) {
+                goodsShow(g);
+            }
 
-            System.out.println("======================");
         }
     }
+    void goodsShow(Goods g){
+        System.out.println("Code: " + g.ID);
+        System.out.println("Type: " + g.type);
+
+        if (g instanceof Shoes)
+            System.out.println("Color: " + ((Shoes) g).color);
+        if (g instanceof ElectricalDevices)
+            System.out.println("Color: " + ((ElectricalDevices) g).color);
+        if (g instanceof Book)
+            System.out.println("Name: " + ((Book) g).name + "\n" + "Writer: " + ((Book) g).writter);
+        if (g instanceof Magazine)
+            System.out.println("Name: " + ((Magazine) g).name + "\n");
+        System.out.println("Producer: " + g.producer);
+        System.out.println("Price: " + g.price);
+        System.out.println("Number: " + g.number);
+
+        System.out.println("======================");
+    }
 }
+
