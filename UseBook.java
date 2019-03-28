5a7165bce4002ce3a36eb4e5a0dc7e5aec866b1d
unknown <hamid.r_torabi@yahoo.com>
2019-03-28 16:30:17 +0430

    Adds a loop for checking entry

--- UseBook.java
+++ UseBook.java
@@ -4,23 +4,26 @@ import java.util.Scanner;
 public class UseBook {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        while (true) {
             System.out.println("This Book is fiction or nonfiction?");
-            if(scanner.nextLine().equalsIgnoreCase("fiction")) {
+            if (scanner.nextLine().equalsIgnoreCase("fiction")) {
                 System.out.println("Enter its title");
-                Fiction book =new Fiction(scanner.nextLine(),Fiction.fictionPrice);
+                Fiction book = new Fiction(scanner.nextLine(), Fiction.fictionPrice);
                 System.out.println(book.title);
                 System.out.println(book.price);
+                break;
             }
-            else{
+            if (scanner.nextLine().equalsIgnoreCase("nonfiction")) {
                 System.out.println("Enter its title");
-                NonFiction book=new NonFiction(scanner.nextLine(),NonFiction.nonFictionPrice);
+                NonFiction book = new NonFiction(scanner.nextLine(), NonFiction.nonFictionPrice);
                 System.out.println(book.title);
                 System.out.println(book.price);
+                break;
 
 
             }
 
-
+        }
 
         }
 
