07042e1d9dd4f2f354c4d63b9e7d35b6f3e6068f
unknown <hamid.r_torabi@yahoo.com>
2019-03-28 17:51:24 +0430

    Adds a seperator for printer

--- MetalElement.java
+++ MetalElement.java
@@ -9,5 +9,6 @@ public class MetalElement extends Element {
         System.out.println("Atomic Number: "+atomicNumber);
         System.out.println("Atomic Weight: "+atomicWeight);
         System.out.println(" metals are good conductors of electricity");
+        System.out.println("=============================================");
     }
 }
