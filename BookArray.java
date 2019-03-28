d46546e96088a1f777e71c2d6a26f4e2f34a52aa
unknown <hamid.r_torabi@yahoo.com>
2019-03-28 16:52:14 +0430

    Adds a loop for checking entry

--- BookArray.java
+++ BookArray.java
@@ -4,16 +4,21 @@ public class BookArray {
         Scanner scanner = new Scanner(System.in);
         Book[] books = new Book[10];
         for (int i = 0; i < 10; i++) {
-            System.out.println("This Book is fiction or nonfiction?");
-            if(scanner.nextLine().equalsIgnoreCase("fiction")) {
-                System.out.println("Enter its title");
-                books[i]=new Fiction(scanner.nextLine(),Fiction.fictionPrice);
+            while (true) {
+                System.out.println("This Book is fiction or nonfiction?");
+                String s=scanner.nextLine();
+                if (s.equalsIgnoreCase("fiction")) {
+                    System.out.println("Enter its title");
+                    books[i] = new Fiction(scanner.nextLine(), Fiction.fictionPrice);
+                    break;
 
-            }
-            else{
-                System.out.println("Enter its title");
-                books[i]=new NonFiction(scanner.nextLine(),NonFiction.nonFictionPrice);
+                }
+                if (s.equalsIgnoreCase("nonfiction")) {
+                    System.out.println("Enter its title");
+                    books[i] = new NonFiction(scanner.nextLine(), NonFiction.nonFictionPrice);
+                    break;
 
+                }
             }
         }
         for (Book book:books)
