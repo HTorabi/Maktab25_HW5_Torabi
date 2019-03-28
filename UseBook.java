bb1453a01b760c2be706e36bd8d9369f66b68d51
unknown <hamid.r_torabi@yahoo.com>
2019-03-28 16:35:02 +0430

    Adds a parameter for saving Entry String

--- UseBook.java
+++ UseBook.java
@@ -6,14 +6,16 @@ public class UseBook {
         Scanner scanner = new Scanner(System.in);
         while (true) {
             System.out.println("This Book is fiction or nonfiction?");
-            if (scanner.nextLine().equalsIgnoreCase("fiction")) {
+            String s=scanner.nextLine();
+
+            if (s.equalsIgnoreCase("fiction")) {
                 System.out.println("Enter its title");
                 Fiction book = new Fiction(scanner.nextLine(), Fiction.fictionPrice);
                 System.out.println(book.title);
                 System.out.println(book.price);
                 break;
             }
-            if (scanner.nextLine().equalsIgnoreCase("nonfiction")) {
+            if (s.equalsIgnoreCase("nonfiction")) {
                 System.out.println("Enter its title");
                 NonFiction book = new NonFiction(scanner.nextLine(), NonFiction.nonFictionPrice);
                 System.out.println(book.title);
