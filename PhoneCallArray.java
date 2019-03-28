d9f6df0cc9c8668c09757de7c4a3f123caa9405f
unknown <hamid.r_torabi@yahoo.com>
2019-03-28 23:14:39 +0430

    Adds conditions for checking

--- PhoneCallArray.java
+++ PhoneCallArray.java
@@ -4,20 +4,26 @@ public class PhoneCallArray {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         PhoneCall[] phoneCalls = new PhoneCall[2];
-        for (int i = 0; i < 2; i++) {
+        int i=0;
+        while (true){
             System.out.println("Incoming or Outgoing Phone Call?");
-            if (scanner.nextLine().equalsIgnoreCase("incoming")) {
+            String s=scanner.nextLine();
+            if (s.equalsIgnoreCase("incoming")) {
                 System.out.println("Enter Phone Number");
                 phoneCalls[i] = new IncomingPhoneCall(scanner.nextLine());
-            } else {
+                i++;
+            }
+            if (s.equalsIgnoreCase("outgoing")) {
                 System.out.println("Enter Phone Number");
                 String phone = scanner.nextLine();
                 System.out.println("Enter Time in Minute");
                 Scanner scanner1=new Scanner(System.in);
                 int time = scanner1.nextInt();
                 phoneCalls[i] = new OutgoingPhoneCall(phone, time);
-
+                i++;
             }
+            if (i==phoneCalls.length)
+                break;
         }
         for (PhoneCall phoneCall:phoneCalls)
             phoneCall.getInformation();
